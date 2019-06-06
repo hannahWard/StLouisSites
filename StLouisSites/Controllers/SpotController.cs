5f0b8ffc006b71aa1647d27c23956d7f094bb195
h.c.ward1991@gmail.com <h.c.ward1991@gmail.com>
2019-06-05 19:57:46 -0500

    Database Class Work Thru

---
+++ StLouisSites/Controllers/SpotController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StLouisSites.Data;
+using StLouisSites.Models;
+
+namespace StLouisSites.Controllers
+{
+    public class SpotController : Controller
+    {
+        private ApplicationDbContext context;
+
+        public SpotController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Spot> spots = context.Spots.ToList();
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Spot spot)
+        {
+            context.Add(spot);
+            context.SaveChanges();
+            return RedirectToAction(nameof (Index));
+        }
+    }
+}
---
+++ StLouisSites/Data/Migrations/20190606001432_AddSpotTable.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StLouisSites.Data.Migrations
+{
+    public partial class AddSpotTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Spots",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Spots", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Spots");
+        }
+    }
+}
